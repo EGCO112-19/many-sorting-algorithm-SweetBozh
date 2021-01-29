0815eebcb5313d064eb0a53e0bedea2ace7c03df
SweetBozh <nalin_hippo@hotmail.com>
2021-01-29 15:37:02 +0000

    Change Bubble

--- bubble.h
+++ bubble.h
@@ -13,18 +13,22 @@ void display(int value[], int N){
 void bubble(int a[], int N){
     int i;
     int j;
+    int round=1;
     int sorted;
-  for(i=N-1;i>0;i--){
+  for(i=N-1;i>0;i--)
+  {
     sorted=0;
-    for(j=0;j<i;j++){
-        if(a[j]>a[j+1]) {// decending
+    for(j=0;j<i;j++)
+    {
+        if(a[j]>a[j+1]) 
+        {// decending
             swap(&a[j],&a[j+1]);
             sorted=1;
         }
         display(a,N);
     }
     
-    printf("\t===end of round %d===\n",i);
+    printf("\t===end of round %d===\n",round++);
     if (sorted==0) break;
   }
     
